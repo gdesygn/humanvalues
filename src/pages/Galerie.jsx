cef894fbba5f17006df5ebd3c3483ee45d83d6d9
GDesygn <contact@gdesygn.com>
2026-06-02 21:48:49 +0200

    feat: Ajout de l'œuvre Ballon de Foot US dans la section Artefacts

--- src/pages/Galerie.jsx
+++ src/pages/Galerie.jsx
@@ -159,7 +159,8 @@ const ailes = [
       { id: "ar36", title: "Home Run", subject: "Baseball", text: "La batte et la balle. Le classique américain forgé par les lettres HV 2025.", image: "/artworks/hv_baseball.jpg" },
       { id: "ar37", title: "Le Strike", subject: "Bowling", text: "La quille et la boule. L'alignement parfait avant l'impact 2025.", image: "/artworks/hv_bowling.jpg" },
       { id: "ar38", title: "Le Caddie", subject: "Golf", text: "Le célèbre véhicule des greens, dessiné par l'empreinte de la collection 2025.", image: "/artworks/caddie.jpg" },
-      { id: "ar39", title: "Ballon de Basket", subject: "Objet Sportif", text: "La sphère orange sculptée par la typographie Human Values 2025.", image: "/artworks/ballon_basket.jpg" }
+      { id: "ar39", title: "Ballon de Basket", subject: "Objet Sportif", text: "La sphère orange sculptée par la typographie Human Values 2025.", image: "/artworks/ballon_basket.jpg" },
+      { id: "ar40", title: "Ballon de Foot US", subject: "Objet Sportif", text: "L'ovale du gridiron, taillé dans l'esprit de l'édition 2025.", image: "/artworks/ballon_us.jpg" }
     ]
   },
   {
